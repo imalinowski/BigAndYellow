323a7e3adbfb9926bb3bd4b0946c6dd4ac1f3f1d
Ilya Malinowski <malinowski221106@gmail.com>
2021-12-11 21:07:33 +0300

    custom ViewModelFactory

--- TinkoffCourseProject/app/src/main/java/com/malinowski/bigandyellow/di/ViewModelFactory.kt
+++ TinkoffCourseProject/app/src/main/java/com/malinowski/bigandyellow/di/ViewModelFactory.kt
@@ -9,28 +9,23 @@ import javax.inject.Inject
 import javax.inject.Provider
 import kotlin.reflect.KClass
 
+/**
+ * @author i.malinowski
+**/
+
 class ViewModelFactory @Inject constructor(
     private val creators: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        var creator: Provider<out ViewModel>? = creators[modelClass]
-        if (creator == null) {
-            for ((key, value) in creators) {
-                if (modelClass.isAssignableFrom(key)) {
-                    creator = value
-                    break
-                }
-            }
+        val creator = creators.filter { (k, _) ->
+            modelClass.isAssignableFrom(k)
+        }.values.firstOrNull().let {
+            if (it == null)
+                throw IllegalArgumentException("Unknown model class: $modelClass")
+            it
         }
-        if (creator == null) {
-            throw IllegalArgumentException("Unknown model class: $modelClass")
-        }
-        try {
-            @Suppress("UNCHECKED_CAST")
-            return creator.get() as T
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
+        @Suppress("UNCHECKED_CAST")
+        return creator.get() as T
     }
 }
 
